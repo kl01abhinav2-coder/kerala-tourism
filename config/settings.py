4d2f6f1c4a265ad484ef712c7bcc96eaae5dfdf9
samithaaa <samithafathima2002@gmail.com>
2026-02-18 12:55:22 +0530

    Allow all hosts and fix port

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = 'django-insecure-change-this-key'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 INSTALLED_APPS = [
